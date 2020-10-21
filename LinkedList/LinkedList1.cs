d698a0c37b2e1a6c11e234c7451455379e3ff8a8
piyush12346789 <piyushgupta024@gmail.com>
2020-10-21 21:53:41 +0530

    [Piyush] Add: Method to insert an element in the linked list.

--- LinkedList/LinkedList1.cs
+++ LinkedList/LinkedList1.cs
@@ -37,6 +37,34 @@ namespace LinkedList
                 n.next = node;
             }
         }
+        public void InsertAtGivenPosition(int position, int data)
+        {
+            Node node = new Node(data);
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            else if (position == 1)
+            {
+                node.next = head;
+                head = node;
+            }
+            else
+            {
+                Node n = head, previous = head, after;
+                for (int pos = 1; pos < position; pos++)
+                {
+                    n = n.next;
+                    if (pos == position - 2)
+                    {
+                        previous = n;
+                    }
+                }
+                after = n;
+                previous.next = node;
+                node.next = after;
+            }
+        }
         public void Display()
         {
             if (head == null)
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,9 +7,9 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedList1 linkedList = new LinkedList1();
-            linkedList.Append(70);
-            linkedList.Append(30);
             linkedList.Append(56);
+            linkedList.Append(70);
+            linkedList.InsertAtGivenPosition(2, 30);
             linkedList.Display();
         }
     }
